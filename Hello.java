8374775d0fca94543bb76f93abba33e134d66f22
Ariel <briceal@plu.edu>
2021-03-09 14:38:01 -0800

    changed the hello method

--- Hello.java
+++ Hello.java
@@ -4,12 +4,7 @@ public class Hello{
 //Comment to break the commit
     public static void main(String args[])
     {
-        Random rand = new Random();
-        int num = rand.nextInt();
-        if(num % 2 == 0)
-         System.out.println("Even Number, Hello World");
-        if(num % 2 != 0)
-            System.out.println("Odd Number, Hello World");
-        System.out.println("Yes");
+        x = 1+1;
+        System.out.println(x);
     }
 }
